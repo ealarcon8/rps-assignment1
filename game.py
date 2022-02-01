ad7fbe41407ec821c8f640170ba3a3e648d1e889
ealarcon8 <eva8@georgetown.edu>
2022-02-01 17:09:33 -0500

    User Input Validation and Quit

--- game.py
+++ game.py
@@ -9,20 +9,45 @@
 
 # ASK FOR USER INPUT
 
-u = input("Please choose one of the following: 'Rock', 'Paper', or 'Scissors': ")
+user_input = input("Please choose one of the following: 'Rock', 'Paper', or 'Scissors': ")
 
-print ("User chose:", u)
+# move after validation
+# print ("User chose:", u)
 
 
 # VALIDATIONS
 
-###ensure valid choices are stored in list datatype
-### arr = ["Rock", "rock", "ROCK", "Paper", "paper", "PAPER", "Scissors", "scissors", "SCISSORS"] 
+if (user_input == "Rock"):
+     print ("User chose:", user_input)
+elif (user_input == "rock"):
+    print ("User chose:", user_input)
+elif (user_input == "ROCK"):
+    print ("User chose:", user_input)
+elif (user_input == "Paper"):
+    print ("User chose:", user_input)
+elif (user_input == "paper"):
+    print ("User chose:", user_input)
+elif (user_input == "PAPER"):
+    print ("User chose:", user_input)
+elif (user_input == "Scissors"):
+    print ("User chose:", user_input)
+elif (user_input == "scissors"):
+    print ("User chose:", user_input)
+elif (user_input == "SCISSORS"):
+    print ("User chose:", user_input)
+#else syntax from: https://www.w3schools.com/python/python_conditions.asp
+else: 
+    print("Sorry, I didn't understand that choice. Please try again.")
+# quit syntax from: https://www.geeksforgeeks.org/python-exit-commands-quit-exit-sys-exit-and-os-_exit/
+    print (quit)
+    quit()
 
 
 # COMPUTER CHOICE
 
+# import random from class
 import random
+#is the same as: from random import choice
 
 options = ["rock", "paper", "scissors"]
 
@@ -30,8 +55,11 @@ computer_choice=random.choice(options)
 
 print("COMPUTER CHOSE:", computer_choice)
 
+
 # DETERMINE THE WINNER
 
 
+
+## Use Eugenie's code from Slack or Brandon's code from Slack (For BRandon's start with "result=none")
 
 # FINAL RESULTS
